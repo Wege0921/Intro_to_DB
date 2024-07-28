80ae9cd660a32076060fdb805d470db10640de91
Wege0921 <wegemuluneh@gmail.com>
2024-07-28 05:20:55 -0700

    task_4.sql

---
+++ task_4.sql
@@ -0,0 +1,14 @@
+-- Select full description of the Books table from information_schema
+SELECT 
+    COLUMN_NAME, 
+    COLUMN_TYPE, 
+    IS_NULLABLE, 
+    COLUMN_KEY, 
+    COLUMN_DEFAULT, 
+    EXTRA 
+FROM 
+    INFORMATION_SCHEMA.COLUMNS 
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' 
+    AND TABLE_NAME = 'Books';
+
